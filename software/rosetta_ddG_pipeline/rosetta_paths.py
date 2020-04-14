0b0d672f1016711e17d86213d1f3eddcf26c1bbb
j0kaso <johanna.tiemann@gmail.com>
2020-04-14 10:43:43 +0200

    [CHANGE] remove string manipulation with path formatting [ADD] add Rosetta database path to environment variables [CHANGE] use shutil copy to store input files

--- software/rosetta_ddG_pipeline/rosetta_paths.py
+++ software/rosetta_ddG_pipeline/rosetta_paths.py
@@ -2,14 +2,16 @@ import os
 
 # yes, I know global vars are bad...
 default_path = {
-    "muscle_exec": '/groups/sbinlab/software/muscle/muscle3.8.31_i86linux64',
-    "ddG_pipeline": '/sbinlab/software/PRISM/software/rosetta_ddG_pipeline',
-    "Rosetta_main_path": '/sbinlab/software/Rosetta_2018_Oct_d557f8/source/',
-    "Rosetta_tools_path": '/sbinlab/software/Rosetta_tools/tools/',
+    'muscle_exec': '/groups/sbinlab/software/muscle/muscle3.8.31_i86linux64',
+    'ddG_pipeline': '/sbinlab/software/PRISM/software/rosetta_ddG_pipeline',
+    'Rosetta_main_path': '/sbinlab/software/Rosetta_2018_Oct_d557f8/source/',
+    'Rosetta_tools_path': '/sbinlab/software/Rosetta_tools/tools/',
+    'Rosetta_database_path': '/sbinlab/software/Rosetta_2018_Oct_d557f8/database/',
 }
 
-#load exec if within environment
+
 def load_env(env):
+    # load exec if within environment
     if os.getenv(env) != None:
         return os.getenv(env)
     else:
@@ -21,17 +23,22 @@ muscle_exec = load_env('muscle_exec')
 ddG_pipeline = load_env('ddG_pipeline')
 Rosetta_main_path = load_env('Rosetta_main_path')
 Rosetta_tools_path = load_env('Rosetta_tools_path')
+Rosetta_database_path = load_env('Rosetta_database_path')
 
-print('current env paths & exec:', muscle_exec, ddG_pipeline, Rosetta_main_path, Rosetta_tools_path)
+print('current env paths & exec:', muscle_exec, ddG_pipeline,
+      Rosetta_main_path, Rosetta_tools_path, Rosetta_database_path)
 
-#Rosetta paths
+# Rosetta paths
 path_to_rosetta = Rosetta_main_path
-path_to_clean_pdb = f"{Rosetta_tools_path}protein_tools/scripts/clean_pdb.py"
-path_to_clean_keep_ligand = f"{path_to_rosetta}src/apps/public/relax_w_allatom_cst/clean_pdb_keep_ligand.py"
-#Personal paths
+path_to_clean_pdb = os.path.join(
+    Rosetta_tools_path, 'protein_tools/scripts/clean_pdb.py')
+path_to_clean_keep_ligand = os.path.join(
+    path_to_rosetta, 'src/apps/public/relax_w_allatom_cst/clean_pdb_keep_ligand.py')
+# Personal paths
 path_to_stability_pipeline = ddG_pipeline
-default_output_path = f"{path_to_stability_pipeline}/output"
-path_to_parameters = f"{path_to_stability_pipeline}/rosetta_parameters"
+default_output_path = os.path.join(path_to_stability_pipeline, 'output')
+path_to_parameters = os.path.join(
+    path_to_stability_pipeline, 'rosetta_parameters')
 
-#Muscle exec
+# Muscle exec
 path_to_muscle = muscle_exec
