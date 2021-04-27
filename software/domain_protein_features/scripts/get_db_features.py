016464b1afa7e60e1dbecc7b9e57c0901a28e69d
Henrike Zschach <henrike.zschach@gmail.com>
2021-04-27 09:25:10 +0200

    added getting mobiDB disorder-priority

--- software/domain_protein_features/scripts/get_db_features.py
+++ software/domain_protein_features/scripts/get_db_features.py
@@ -40,7 +40,7 @@ def get_disprot_disordered(uniprot_id, server = 'https://disprot.org/api/'):
 				disordered_region.append([elem['start'],elem['end'],elem['type']])
 			return(disordered_region)
 
-def get_mobidb_disordered(uniprot_id, server = 'https://mobidb.bio.unipd.it/api/'):
+def get_mobidb_lite_disordered(uniprot_id, server = 'https://mobidb.bio.unipd.it/api/'):
 	"""
 	Extracts disorder content (%) from mobidb: https://mobidb.bio.unipd.it/help/apidoc
 	for present uniprot_ID
@@ -73,6 +73,48 @@ def get_mobidb_disordered(uniprot_id, server = 'https://mobidb.bio.unipd.it/api/
 		#if this key doesn't exist there are no disordered regions predicted by mobiDB-lite	
 		else:
 			return('NA')
+
+def get_mobidb_consensus_priority(uniprot_id, server = 'https://mobidb.bio.unipd.it/api/'):
+	"""
+	Extracts disordered regions for present uniprot_ID with 'priority' as a consensus source, compare https://mobidb.bio.unipd.it/about/mobidb "Feature identifiers"
+	"""
+	
+	#go through the evidence levels and take the highest one available
+	evidence_levels = ['curated', 'derived', 'homology', 'prediction']
+	
+	for el in evidence_levels:
+		
+		#we're only interested in the regions field
+		ext = "download?acc="+uniprot_id+"&projection="+el+"-disorder-priority.regions"
+		try:
+			r = requests.get(server+ext, headers={ "Content-Type" : "application/json"})
+			r.raise_for_status()
+		except HTTPError as e:
+			status_code = r.status_code
+			print('While looking up', uniprot_id, 'on mobiDB the following error occured:',status_code)
+			#P0DTE7 produced a code 500, internal server error. I checked and other uniprot IDs still work fine, so I'm not sure if it is a temporary issue. For now, I think we should return NA
+			if status_code == 500:
+				return('NA')
+			sys.exit()
+			
+
+		else:
+			#download returns a list of jsons which in our case has only one entry since we're only asking for a single uniprot ID 
+			contents = r.json()[0]
+			#if this json is empty, the requested evidence level doesn't exist. Try with the next one
+			if not contents:
+				continue
+				
+			else:
+				disordered_region = []
+				for elem in contents[el+"-disorder-priority"]['regions']:
+					#disordered_region.append([elem[0],elem[1]])
+					disordered_region.append(elem)
+				return(disordered_region)
+				
+	#if we went through all four evidence levels and haven't returned anything, return NA now
+	return('NA')
+
 
 def get_disprot_disordered_orig(uniprot_id):
     """
