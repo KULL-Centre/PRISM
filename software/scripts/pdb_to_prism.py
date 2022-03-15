edd0ec35481a5adb5cb4b2a2e8f79bb5099eca8e
j0kaso <johanna.tiemann@gmail.com>
2022-03-15 17:02:04 +0100

    [prepare] bugfix of previous commit

--- software/scripts/pdb_to_prism.py
+++ software/scripts/pdb_to_prism.py
@@ -406,7 +406,7 @@ def pdb_renumb(pdb_input, output_dir=None, keepchain='all', chainorder=None, kee
         with open(pdb_input, 'r') as fp:
             if keep_ligand:
                 for line in fp:
-                    if line.split()[0]=='MODEL' and line.split()[0]=='2':
+                    if line.split()[0]=='MODEL' and line.split()[1]=='2':
                         break
                     if (line[17:20] == keep_ligand) and (line[:6] in ['HETATM', 'ATOM  ']):
                         resstring = line[22:27]
@@ -427,7 +427,7 @@ def pdb_renumb(pdb_input, output_dir=None, keepchain='all', chainorder=None, kee
                         fp2.write(new_line)
             else:
                 for line in fp:
-                    if line.split()[0]=='MODEL' and line.split()[0]=='2':
+                    if line.split()[0]=='MODEL' and line.split()[1]=='2':
                         break
                     if line[0:4] == "ATOM":
                         chain = line[21]
